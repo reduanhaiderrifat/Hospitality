a690154841de9a004cd9ac929e269c4ea3c3c8df
reduanhaiderrifat <reduanhaiderrifat@gmail.com>
2024-04-14 20:42:53 +0600

    do dynamic website name for comments routes

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import CommentsCard from "../components/CommentsCard";
 import { AuthContext } from "../AuthProvider/AuthProvider";
+import { Helmet } from "react-helmet-async";
 const Comments = () => {
   const { user } = useContext(AuthContext);
   const [comments, setComments] = useState(() => {
@@ -31,58 +32,63 @@ const Comments = () => {
   };
 
   return (
-    <div className="w-full md:w-1/2 lg:w-1/2 mx-auto">
-      <h2 className="text-center text-3xl font-bold my-12">
-        Add your experience about our website and our property dealing
-      </h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="grid grid-cols-1 gap-5 mb-32">
-          <input
-            type="text"
-            className="border rounded-lg border-black p-4"
-            name="name"
-            value={newComment.name}
-            onChange={handleInputChange}
-            placeholder="Your Name"
-            required={!user}
-          />
-          <input
-            type="email"
-            className="border rounded-lg border-black p-4"
-            name="email"
-            value={newComment.email}
-            onChange={handleInputChange}
-            placeholder="Your Email"
-            required={!user}
-          />
-          <textarea
-            className="border rounded-lg border-black p-4"
-            rows={10}
-            cols={70}
-            name="comment"
-            value={newComment.comment}
-            onChange={handleInputChange}
-            placeholder="Type your comment here..."
-            required
-          ></textarea>
-          <button className="btn w-1/2" type="submit">
-            Add Comment
-          </button>
-        </div>
-      </form>
-      <h1 className=" text-2xl font-semibold">Comments :</h1>
-      <div className="border rounded-lg border-black p-2 space-y-4">
-        {comments.map((comment, index) => (
-          <div key={index}>
-            <CommentsCard
-              comment={comment.comment}
-              name={comment.name}
-              email={comment.email}
-            ></CommentsCard>
+    <>
+      <Helmet>
+        <title>Hospitality_Comments</title>
+      </Helmet>
+      <div className="w-full md:w-1/2 lg:w-1/2 mx-auto">
+        <h2 className="text-center text-3xl font-bold my-12">
+          Add your experience about our website and our property dealing
+        </h2>
+        <form onSubmit={handleFormSubmit}>
+          <div className="grid grid-cols-1 gap-5 mb-32">
+            <input
+              type="text"
+              className="border rounded-lg border-black p-4"
+              name="name"
+              value={newComment.name}
+              onChange={handleInputChange}
+              placeholder="Your Name"
+              required={!user}
+            />
+            <input
+              type="email"
+              className="border rounded-lg border-black p-4"
+              name="email"
+              value={newComment.email}
+              onChange={handleInputChange}
+              placeholder="Your Email"
+              required={!user}
+            />
+            <textarea
+              className="border rounded-lg border-black p-4"
+              rows={10}
+              cols={70}
+              name="comment"
+              value={newComment.comment}
+              onChange={handleInputChange}
+              placeholder="Type your comment here..."
+              required
+            ></textarea>
+            <button className="btn w-1/2" type="submit">
+              Add Comment
+            </button>
           </div>
-        ))}
+        </form>
+        <h1 className=" text-2xl font-semibold">Comments :</h1>
+        <div className="border rounded-lg border-black p-2 space-y-4">
+          {comments.map((comment, index) => (
+            <div key={index}>
+              <CommentsCard
+                comment={comment.comment}
+                name={comment.name}
+                email={comment.email}
+              ></CommentsCard>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
